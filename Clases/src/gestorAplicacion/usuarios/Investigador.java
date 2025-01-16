48d17fca7b0f0d36c78469b93a17fd68099e77b0
DanyFz <138801876+DanyFz@users.noreply.github.com>
2025-01-15 22:12:29 -0500

    a

--- Clases/src/gestorAplicacion/usuarios/Investigador.java
+++ Clases/src/gestorAplicacion/usuarios/Investigador.java
@@ -6,3 +6,17 @@ public class Investigador extends Usuario{
     private DoubleList Equipos;
 
 }
+public void GenerarInventario{
+    fileName = "Inventario.txt";
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        DoubleNode fir = Usuario.equipos.first();
+        while (fir != null) {
+            writer.write(fir.getData().toString());
+            writer.newLine();
+            fir = fir.getNext();
+        }
+    } catch (IOException e) {
+        System.out.println("Error al escribir en el archivo: " + fileName);
+    }
+
