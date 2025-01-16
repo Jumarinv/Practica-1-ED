039e9e5f88a4a5732fa04bd09d17832bc717aefe
Sebastian Guerra Guerra <159485822+SebasGuerrag1@users.noreply.github.com>
2025-01-15 22:13:11 -0500

    .

--- Clases/src/gestorAplicacion/usuarios/Investigador.java
+++ Clases/src/gestorAplicacion/usuarios/Investigador.java
@@ -1,5 +1,6 @@
 package gestorAplicacion.usuarios;
 import gestorAplicacion.administacion.Equipo;
+import gestorAplicacion.extras.Fecha;
 import gestorAplicacion.listas.DoubleList;
 
 public class Investigador {
@@ -7,5 +8,10 @@ public class Investigador {
 
     public Investigador(Usuario investigador) {
         this.investigador = investigador;
+    }
+
+    public void solicitudAgregarEquipo(String nombre, long placa, Fecha fechaCompra, long precio){
+        Equipo equipoNuevo = new Equipo(nombre, placa,fechaCompra, precio,investigador.getId());
+        System.out.println("Solicitud de agregar equipo: " + equipoNuevo.toString());
     }
 }
