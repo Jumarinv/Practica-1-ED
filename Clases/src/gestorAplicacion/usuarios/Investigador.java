4186dbc529cdc314d36ffe40ae2de64bdc92c891
Juan Camilo Marin Valencia <159485801+Jumarinv@users.noreply.github.com>
2025-01-15 22:26:17 -0500

    Correccion investigador

--- Clases/src/gestorAplicacion/usuarios/Investigador.java
+++ Clases/src/gestorAplicacion/usuarios/Investigador.java
@@ -10,22 +10,27 @@ public class Investigador {
         this.investigador = investigador;
     }
 
-    public void solicitudAgregarEquipo(String nombre, long placa, Fecha fechaCompra, long precio){
-        Equipo equipoNuevo = new Equipo(nombre, placa,fechaCompra, precio,investigador.getId());
+    public void solicitudAgregarEquipo(String nombre, long placa, Fecha fechaCompra, long precio) {
+        Equipo equipoNuevo = new Equipo(nombre, placa, fechaCompra, precio, investigador.getId());
         System.out.println("Solicitud de agregar equipo: " + equipoNuevo.toString());
     }
-}
-public void GenerarInventario{
-    fileName = "Inventario.txt";
 
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-        DoubleNode fir = investigador.equipos.first();
-        while (fir != null) {
-            writer.write(fir.getData().toString());
-            writer.newLine();
-            fir = fir.getNext();
+    public void GenerarInventario
+
+    {
+        fileName = "Inventario.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            DoubleNode fir = investigador.equipos.first();
+            while (fir != null) {
+                writer.write(fir.getData().toString());
+                writer.newLine();
+                fir = fir.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo: " + fileName);
         }
-    } catch (IOException e) {
-        System.out.println("Error al escribir en el archivo: " + fileName);
+
     }
+}
 
