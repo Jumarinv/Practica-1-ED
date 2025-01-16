7b9919b1f0dd890397abbdec457be037b198fe83
Juan Camilo Marin Valencia <159485801+Jumarinv@users.noreply.github.com>
2025-01-16 03:50:17 -0500

    Cambios

--- Clases/src/gestorAplicacion/usuarios/Funcionalidad.java
+++ Clases/src/gestorAplicacion/usuarios/Funcionalidad.java
@@ -20,149 +20,164 @@ public class Funcionalidad {
     private static Scanner scann = new Scanner (System.in);
 
     public static void mostrarOpciones(String tipoUsuario, Usuario usuario) throws IOException {
-        if (tipoUsuario.equalsIgnoreCase("investigador")) {
-            System.out.println("Opciones para Investigador:");
-            System.out.println("1. Consultar lista de equipos en su inventario.");
-            System.out.println("2. Solicitar agregar un nuevo equipo.");
-            System.out.println("3. Solicitar eliminar un equipo.");
-            System.out.println("4. Consultar el estado de sus solicitudes.");
-            System.out.println("5. Generar archivo con su inventario.");
-            System.out.println("6. Generar archivo con el estado de sus solicitudes.");
 
-            int opcion = scann.nextInt();
+        boolean status = true;
+        while (status) {
+            if (tipoUsuario.equalsIgnoreCase("investigador")) {
+                System.out.println("Opciones para Investigador:");
+                System.out.println("1. Consultar lista de equipos en su inventario.");
+                System.out.println("2. Solicitar agregar un nuevo equipo.");
+                System.out.println("3. Solicitar eliminar un equipo.");
+                System.out.println("4. Consultar el estado de sus solicitudes.");
+                System.out.println("5. Generar archivo con su inventario.");
+                System.out.println("6. Generar archivo con el estado de sus solicitudes.");
+                System.out.println("7. Cerrar sesion");
 
-            switch (opcion) {
+                int opcion = scann.nextInt();
 
-                case 1:
-                    mostrarInventario(usuario);
-                    break;
+                switch (opcion) {
 
-                case 2:
-                    System.out.println("Ingresa los datos (nombre, placa, fechaCompra, precio) separados por comas:");
+                    case 1:
+                        mostrarInventario(usuario);
+                        break;
 
-                    String input = scann.nextLine();
-                    String[] datos = input.split(",");
-                    short dd= Short.parseShort(datos[2]);
-                    short mm=Short.parseShort(datos[3]);
-                    short aa=Short.parseShort(datos[4]);
-                    Fecha fechaCompra = new Fecha(dd,mm,aa);
-                    solicitudAgregarEquipo(usuario,datos[0], Long.parseLong(datos[1]), fechaCompra,Long.parseLong(datos[5]));
+                    case 2:
+                        System.out.println("Ingresa los datos (nombre, placa, fechaCompra, precio) separados por comas:");
 
-                    break;
+                        String input = scann.nextLine();
+                        String[] datos = input.split(",");
+                        short dd = Short.parseShort(datos[2]);
+                        short mm = Short.parseShort(datos[3]);
+                        short aa = Short.parseShort(datos[4]);
+                        Fecha fechaCompra = new Fecha(dd, mm, aa);
+                        solicitudAgregarEquipo(usuario, datos[0], Long.parseLong(datos[1]), fechaCompra, Long.parseLong(datos[5]));
 
-                case 3:
-                    System.out.println("Ingrese el numero de la placa del equipo que desea eliminar");
-                    Long placa = scann.nextLong();
-                    solicitudEliminarEquipo(usuario,placa);
+                        break;
 
-                    break;
+                    case 3:
+                        System.out.println("Ingrese el numero de la placa del equipo que desea eliminar");
+                        Long placa = scann.nextLong();
+                        solicitudEliminarEquipo(usuario, placa);
 
-                case 4:
-                    MostrarEstadoSolicitud(usuario);
-                    break;
+                        break;
 
-                case 5:
-                    GenerarInventario(usuario);
-                    break;
+                    case 4:
+                        MostrarEstadoSolicitud(usuario);
+                        break;
 
-                case 6:
-                    GenerarSolicitud(usuario);
-                    break;
+                    case 5:
+                        GenerarInventario(usuario);
+                        break;
+
+                    case 6:
+                        GenerarSolicitud(usuario);
+                        break;
+
+                    case 7:
+
+                        status = false;
+                        break;
+
+                }
+
+
+            } else if (tipoUsuario.equalsIgnoreCase("administrador")) {
+                System.out.println("Opciones para Administrador:");
+                System.out.println("1. Consultar lista de equipos en su inventario.");
+                System.out.println("2. Solicitar agregar un nuevo equipo.");
+                System.out.println("3. Solicitar eliminar un equipo.");
+                System.out.println("4. Registrar un nuevo usuario.");
+                System.out.println("5. Cambiar contraseñas.");
+                System.out.println("6. Eliminar un usuario.");
+                System.out.println("7. Responder solicitudes de investigadores.");
+                System.out.println("8. Generar inventario de investigador");
+                System.out.println("9. Generar inventario del centro de investigación");
+                System.out.println("10. Generar archivo de control de cambios");
+                System.out.println("11. Generar archivo de solicitudes pendientes para agregar");
+                System.out.println("12. Generar archivo de solicitudes pendientes para eliminar");
+                System.out.println("13. Consultar control de cambios");
+
+                int opcion = scann.nextInt();
+
+                switch (opcion) {
+
+                    case 1:
+
+                        mostrarLista(usuario);
+
+                        break;
+
+                    case 2:
+                        System.out.println("Ingresa los datos (nombre, placa, fechaCompra, precio) separados por comas:");
+                        String input = scann.nextLine();
+                        String[] datos = input.split(",");
+                        short dd = Short.parseShort(datos[2]);
+                        short mm = Short.parseShort(datos[3]);
+                        short aa = Short.parseShort(datos[4]);
+                        Fecha fechaCompra = new Fecha(dd, mm, aa);
+                        solicitudAgregarEquipo(usuario, datos[0], Long.parseLong(datos[1]), fechaCompra, Long.parseLong(datos[5]));
+
+                        break;
+
+                    case 3:
+                        System.out.println("Ingrese el numero de la placa del equipo que desea eliminar");
+                        Long placa = scann.nextLong();
+                        solicitudEliminarEquipo(usuario, placa);
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+                        System.out.println("Ingrese id de usuario y la nueva contraseña separada por comas para realizar el cambio de contraseña");
+                        String entrada = scann.nextLine();
+                        String[] datos1 = entrada.split(",");
+                        cambiarContrasena(datos1[0], datos1[1], "Password.txt");
+
+                        break;
+
+                    case 6:
+                        System.out.println("Ingrese el id del usuario que desea eliminar");
+                        String entrada1 = scann.nextLine();
+                        eliminarUsuario(entrada1, "Empleado.txt");
+
+                        break;
+
+                    case 7:
+
+                        break;
+
+                    case 8:
+                        Generarinventarioinvestigador(usuario);
+                        break;
+                    case 9:
+                        GenerarinventarioGeneral();
+                        break;
+                    case 10:
+                        GenerarControlCambios(getC());
+                        break;
+                    case 11:
+                        GenerarPendientesAgregar(getA());
+                        break;
+                    case 12:
+                        GenerarPendientesEliminar(getE());
+                        break;
+                    case 13:
+                        ConsultarCambios(getC());
+                        break;
+
+                    case 14:
+                        status = false;
+                        break;
+
+                }
+
+            } else {
+                System.out.println("Tipo de usuario no válido. Por favor, ingrese 'investigador' o 'administrador'.");
 
             }
-
-
-        } else if (tipoUsuario.equalsIgnoreCase("administrador")) {
-            System.out.println("Opciones para Administrador:");
-            System.out.println("1. Consultar lista de equipos en su inventario.");
-            System.out.println("2. Solicitar agregar un nuevo equipo.");
-            System.out.println("3. Solicitar eliminar un equipo.");
-            System.out.println("4. Registrar un nuevo usuario.");
-            System.out.println("5. Cambiar contraseñas.");
-            System.out.println("6. Eliminar un usuario.");
-            System.out.println("7. Responder solicitudes de investigadores.");
-            System.out.println("8. Generar inventario de investigador");
-            System.out.println("9. Generar inventario del centro de investigación");
-            System.out.println("10. Generar archivo de control de cambios");
-            System.out.println("11. Generar archivo de solicitudes pendientes para agregar");
-            System.out.println("12. Generar archivo de solicitudes pendientes para eliminar");
-            System.out.println("13. Consultar control de cambios");
-
-            int opcion = scann.nextInt();
-
-            switch (opcion) {
-
-                case 1:
-
-                    mostrarLista(usuario);
-
-                    break;
-
-                case 2:
-                    System.out.println("Ingresa los datos (nombre, placa, fechaCompra, precio) separados por comas:");
-                    String input = scann.nextLine();
-                    String[] datos = input.split(",");
-                    short dd= Short.parseShort(datos[2]);
-                    short mm=Short.parseShort(datos[3]);
-                    short aa=Short.parseShort(datos[4]);
-                    Fecha fechaCompra = new Fecha(dd,mm,aa);
-                    solicitudAgregarEquipo(usuario,datos[0], Long.parseLong(datos[1]), fechaCompra,Long.parseLong(datos[5]));
-
-                    break;
-
-                case 3:
-                    System.out.println("Ingrese el numero de la placa del equipo que desea eliminar");
-                    Long placa = scann.nextLong();
-                    solicitudEliminarEquipo(usuario,placa);
-
-                    break;
-
-                case 4:
-
-                    break;
-
-                case 5:
-                    System.out.println("Ingrese id de usuario y la nueva contraseña separada por comas para realizar el cambio de contraseña");
-                    String entrada = scann.nextLine();
-                    String[] datos1 = entrada.split(",");
-                    cambiarContrasena(datos1[0], datos1[1], "Password.txt");
-
-                    break;
-
-                case 6:
-                    System.out.println("Ingrese el id del usuario que desea eliminar");
-                    String entrada1 = scann.nextLine();
-                    eliminarUsuario(entrada1, "Empleado.txt");
-
-                    break;
-
-                case 7:
-
-                    break;
-
-                case 8:
-                     Generarinventarioinvestigador(usuario);
-                    break;
-                case 9:
-                    GenerarinventarioGeneral();
-                    break;
-                case 10:
-                    GenerarControlCambios(getC());
-                    break;
-                case 11:
-                    GenerarPendientesAgregar(getA());
-                    break;
-                case 12:
-                    GenerarPendientesEliminar(getE());
-                    break;
-                case 13:
-                    ConsultarCambios(getC());
-                    break;
-
-            }
-
-        } else {
-            System.out.println("Tipo de usuario no válido. Por favor, ingrese 'investigador' o 'administrador'.");
         }
     }
 
